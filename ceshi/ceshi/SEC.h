2a714e74a551e7529b608a93d721a0b9dd1ba508
hefs <747254346@qq.com>
2015-10-22 02:22:56 +0800

    AAA

---
+++ ceshi/ceshi/SEC.h
@@ -0,0 +1,13 @@
+//
+//  SEC.h
+//  ceshi
+//
+//  Created by hefusang on 15/10/22.
+//  Copyright (c) 2015年 wemedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SEC : UIViewController
+
+@end
---
+++ ceshi/ceshi/SEC.m
@@ -0,0 +1,13 @@
+//
+//  SEC.m
+//  ceshi
+//
+//  Created by hefusang on 15/10/22.
+//  Copyright (c) 2015年 wemedev. All rights reserved.
+//
+
+#import "SEC.h"
+
+@implementation SEC
+
+@end
