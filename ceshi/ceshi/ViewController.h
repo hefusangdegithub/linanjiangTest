181fa8e33d4245393e262daa8c3445148af2b8db
hefusang <hefusang@mirrtalk.com>
2015-10-21 17:01:45 +0800

    hefs

---
+++ ceshi/ceshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshi
+//
+//  Created by wemedev on 15/10/21.
+//  Copyright (c) 2015年 wemedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ceshi/ceshi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ceshi
+//
+//  Created by wemedev on 15/10/21.
+//  Copyright (c) 2015年 wemedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
